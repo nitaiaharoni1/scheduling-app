43eb60d982dead55bf53a993b75a5305df668c10
Nitai <nitaiaharoni1@gmail.com>
2019-11-23 14:30:55 +0200

    init

--- src/components/CalendarCustom.js
+++ src/components/CalendarCustom.js
@@ -1,24 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import {Calendar, momentLocalizer} from 'react-big-calendar'
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
-
 import moment from 'moment';
+import uuid from 'uuid';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
+import '../styles/CalendarCustom.css';
 
-import '../styles/CalendarCustom1.css';
+const DragAndDropCalendar = withDragAndDrop(Calendar);
+const localizer = momentLocalizer(moment);
+localizer.formats.timeGutterFormat = 'H:mm';
 
 export const CalendarCustom = (props) => {
     const [events, setEvents] = useState([]);
     const [time, setTime] = useState(moment().format('H:mm'));
-
-    const localizer = momentLocalizer(moment);
-    localizer.formats.timeGutterFormat = 'H:mm';
-    const btnStyle = {
-        "border-radius": "50%",
-        "opacity": "50%",
-        "padding-left": "10px",
-        "padding-right": "10px"
-    };
 
     useEffect(() => {
         setInterval(() =>
@@ -26,27 +20,42 @@ export const CalendarCustom = (props) => {
             1000 * 60);
     }, []);
 
-    const handleSelect = ({start, end}) => {
-        const title = window.prompt('New Event name');
-        if (title){
-            setEvents([...events, {start, end, title}])
+    const handleSelectEvent = (event) => {
+        console.log(events);
+        const title = window.prompt('Edit event\'s name:');
+        if(title){
+            const updatedEvents = [...events].map(existingEvent => {
+                return existingEvent.id === event.id ? {...existingEvent, title} : existingEvent
+            });
+            setEvents(updatedEvents)
         }
+    };
+
+    const handleSelectSlot = ({start, end}) => {
+        const id = uuid();
+        const title = window.prompt('Enter a new event\'s name:');
+        if(title){
+            setEvents([...events, {id, start, end, title}])
+        }
+    };
+
+    const handleChange = ({event, start, end,id}) => {
+        console.log(events);
+        const updatedEvents = [...events].map(existingEvent => {
+            return existingEvent.id === event.id ? {...existingEvent, start, end} : existingEvent
+        });
+        setEvents(updatedEvents)
     };
 
     return (
         <div className="container p-0 position-relative">
             <div className="h6 text-white position-absolute ml-2 mt-1"> {time}</div>
             <div className="d-flex justify-content-center">
-                <button type="button" className="btn btn-outline-light btn-sm mr-2" style={btnStyle}>
-                    <i className="fas fa-chevron-left fa-sm"></i>
-                </button>
-                <div className="h6 text-white text-muted my-auto font-weight-bold">{props.roomName}</div>
-                <button type="button" className="btn btn-outline-light btn-sm ml-2" style={btnStyle}>
-                    <i className="fas fa-chevron-right fa-sm"></i>
-                </button>
+                <div className="h5 text-white my-auto font-weight-bold">{props.roomName}</div>
             </div>
-            <Calendar
+            <DragAndDropCalendar
                 selectable
+                resizable
                 localizer={localizer}
                 events={events}
                 views={['day']}
@@ -54,8 +63,11 @@ export const CalendarCustom = (props) => {
                 style={{height: 300}}
                 timeslots={2}
                 step={30}
-                onSelectEvent={event => alert(event.title)}
-                onSelectSlot={handleSelect}
+                onEventResize={handleChange}
+                onSelectEvent={handleSelectEvent}
+                onEventDrop={handleChange}
+                onSelectSlot={handleSelectSlot}
+                defaultDate={new Date()}
             />
         </div>
     );
