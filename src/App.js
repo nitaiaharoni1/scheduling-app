6b6a7299b45ea4eb429c5adb69163f9b7f516f07
I355539 <nitai.aharoni@sap.com>
2019-11-24 18:51:32 +0200

    New changelist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { RoomsPage } from "./components/pages/RoomsPage";
-import { NavBar } from "./components/NavBar";
+import { Login } from "./components/pages/Login";
 
 function App() {
-  return (
-    <div className="App">
-        <NavBar/>
-        <RoomsPage/>
-    </div>
-  );
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        // Update useEffect
+    }, []);
+
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Login/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/pages/RoomsPage.js
+++ src/components/pages/RoomsPage.js
@@ -1,16 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import {RoomTablet} from "../RoomTablet"
+import React, { useState, useEffect } from 'react';
+import { RoomTablet } from "../RoomTablet"
+import { NavBar } from "../NavBar";
 
 export const RoomsPage = () => {
     const rooms = ["Alpha"/*, "Bravo", "Charlie"*/];
 
     return (
-        <div className="row d-flex justify-content-around m-5">
-            {rooms.map(room =>
-                <div className="col d-flex justify-content-center">
-                    <RoomTablet roomName={room}/>
-                </div>
-            )}
-        </div>
+        <>
+            <NavBar/>
+            <div className="row d-flex justify-content-around m-5">
+                {rooms.map(room =>
+                    <div className="col d-flex justify-content-center">
+                        <RoomTablet roomName={room}/>
+                    </div>
+                )}
+            </div>
+        </>
     );
 };
