148b3919531910703a1ed142d0217a56e9dc9897
I355539 <nitai.aharoni@sap.com>
2019-12-04 12:41:03 +0200

    New changelist

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -38,48 +38,54 @@ export const Login = (props) => {
     };
 
     return (
-        <div className="container mt-5 pt-3">
+        <div className="container px-2 px-md-4 mt-5 pt-3">
             <div className="row m-0">
-                <div className='col border-right'>
-                    <div className='h3 text-center text-uppercase font-weight-bold'>
-                        Login
+                <div className='col border-right px-2 px-md-4'>
+                    <div className="h-100 d-flex flex-column justify-content-between">
+                        <div className='h3 text-center text-uppercase font-weight-bold'>
+                            Login
+                        </div>
+                        <form onSubmit={handleSubmit} className='mt-4'>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input name="email" type="email" value={email} onChange={handleEmailChange} className="form-control"
+                                       placeholder="Enter your email"
+                                       required/>
+                            </div>
+
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input name="password" value={password} onChange={handlePasswordChange} className="form-control" type="password"
+                                       placeholder="Enter your password" required/>
+                            </div>
+                            <div className="form-group ml-4">
+                                <label className="form-check-label">
+                                    <input onChange={handleCheckboxChange} className="form-check-input" type="checkbox"/>
+                                    Remember me
+                                </label>
+                            </div>
+
+                            <div className="text-center">
+                                <button className="btn btn-dark btn-block">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                    <form onSubmit={handleSubmit} className='mt-4'>
-                        <div className="form-group">
-                            <label>Email</label>
-                            <input name="email" type="email" value={email} onChange={handleEmailChange} className="form-control"
-                                   placeholder="Enter your email"
-                                   required/>
-                        </div>
-
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input name="password" value={password} onChange={handlePasswordChange} className="form-control" type="password"
-                                   placeholder="Enter your password" required/>
-                        </div>
-                        <div className="form-group form-inline">
-                            <input onChange={handleCheckboxChange} className="form-check-input" type="checkbox"/>
-                            <label className="form-check-label">Remember me</label>
-                        </div>
-
-                        <div className="text-center">
-                            <button className="btn btn-dark btn-block">
-                                Login
-                            </button>
-                        </div>
-                    </form>
 
                 </div>
 
-                <div className='col position-relative'>
-                    <div className='h3 text-uppercase font-weight-bold text-center'>
-                        new user?
+                <div className='col px-2 px-md-4'>
+                    <div className="h-100 d-flex flex-column justify-content-between">
+                        <div className='h3 text-uppercase font-weight-bold text-center'>
+                            new user?
+                        </div>
+                        <Link to="/signup" className="mb-0">
+                            <button className='mb-0 btn btn-dark btn-block'>
+                                Signup
+                            </button>
+                        </Link>
                     </div>
-                    <Link to="/signup">
-                        <button style={{bottom: 0}} className='btn btn-dark btn-block position-absolute'>
-                            Signup
-                        </button>
-                    </Link>
                 </div>
             </div>
         </div>
